5f4efcda67b5b62e00670f890240f8b91b113bfc
masoudmzb <masoudparpanchi@gmail.com>
2021-11-21 18:04:58 +0330

    Ngram Function function for creating ngrams created

--- features/visualization_features.py
+++ features/visualization_features.py
@@ -1,1 +1,3 @@
 from visualization_features_masoud import plot_ngram
+
+plot_ngram('سلام من مسعودم سلام من مسعودم سلام من مسعودم گوز')
--- features/visualization_features_masoud.py
+++ features/visualization_features_masoud.py
@@ -5,6 +5,7 @@ from bidi.algorithm import get_display
 import arabic_reshaper
 import matplotlib.pyplot as plt
 import seaborn as sns
+import os
 
 
 def plot_ngram(data, col="not_set_yet", n_gram=2, output_name='n_gram_plot', n_most=5):
@@ -27,7 +28,7 @@ def plot_ngram(data, col="not_set_yet", n_gram=2, output_name='n_gram_plot', n_m
   if isinstance(data, pd.DataFrame):
     if col == "not_set_yet" : assert print("please set which column you want to check")
     the_corpus = data[col]
-  vec = CountVectorizer(ngram_range=(n, n)).fit(the_corpus)
+  vec = CountVectorizer(ngram_range=(n_gram, n_gram)).fit(the_corpus)
   bag_of_words = vec.transform(the_corpus)
   sum_words = bag_of_words.sum(axis=0)
   words_freq = [(get_display(arabic_reshaper.reshape(word))  , sum_words[0, idx]) for word, idx in vec.vocabulary_.items()]
@@ -37,8 +38,10 @@ def plot_ngram(data, col="not_set_yet", n_gram=2, output_name='n_gram_plot', n_m
   g = sns.barplot(y="n-gram", x="count", data=pd.DataFrame(words_freq2[:n_most], columns=['n-gram','count']))
   plt.xticks(rotation=70)
   fig = g.get_figure()
+  if not os.path.exists('./plots_images/'):
+    os.makedirs('./plots_images/')
   # bbox_inches is for showing all the plot parts.
-  fig.savefig(f'./{output_name}.jpg' , bbox_inches="tight")
+  fig.savefig(f'./plots_images/{output_name}.jpg' , bbox_inches="tight")
   return "plot is ready"
 
 
