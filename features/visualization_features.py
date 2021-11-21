a2977a7af4b71546ceecefa15aec28faee9d22ac
masoudmzb <masoudparpanchi@gmail.com>
2021-11-21 19:04:45 +0330

    RareWords Function addded funtion to create rare words plot implemented

--- features/visualization_features.py
+++ features/visualization_features.py
@@ -1,3 +1,4 @@
 from visualization_features_masoud import plot_ngram
-
-plot_ngram('سلام من مسعودم سلام من مسعودم سلام من مسعودم گوز')
+from visualization_features_masoud import plot_rare_words
+# plot_ngram('سلام من مسعودم سلام من مسعودم سلام من مسعودم گوز')
+plot_rare_words("سلام من سلام من سالم من یک دو سه چهار هشت", col='text')
--- features/visualization_features_masoud.py
+++ features/visualization_features_masoud.py
@@ -1,3 +1,4 @@
+#coding=utf-8
 from sklearn.feature_extraction.text import CountVectorizer
 import pandas as pd
 from collections import Counter
@@ -8,11 +9,12 @@ import seaborn as sns
 import os
 
 
+
 def plot_ngram(data, col="not_set_yet", n_gram=2, output_name='n_gram_plot', n_most=5):
   """
   this function will plot most reapeted ngrams for you
   parameters : 
-    data : the data you send to get the nfram from, you can send list of strings, one string, or dataframe 
+    data : the data you send to get the ngram from, you can send list of strings, one string, or dataframe 
     but be aware if you send daataframe you must fill col param too
 
     col : name of the column you want to get ngram from
@@ -44,4 +46,41 @@ def plot_ngram(data, col="not_set_yet", n_gram=2, output_name='n_gram_plot', n_m
   fig.savefig(f'./plots_images/{output_name}.jpg' , bbox_inches="tight")
   return "plot is ready"
 
+def plot_rare_words(data, col="not_set_yet", from_row=0, to_row=5, output_name='rare_words_plot'):
+  """
+  this function will plot most reapeted ngrams for you
+  parameters : 
+    data : the data you send to get the rarewords from, you can send one string, list of strings, or dataframe 
+    but be aware if you send daataframe you must fill col param too
 
+    col : name of the column you want to get ngram from
+
+    from_row : the words are sorted by their occurrence. how many rare words you want to see?  [this row ,to number]
+    to_row : the words are sorted by their occurrence. how many rare words you want to see? [this row ,to number]
+    output_name : name of the jpg file you will get
+
+  """
+  the_corpus = data
+  if isinstance(data, str):
+    the_corpus = pd.DataFrame([data], columns=['text'])
+    col = 'text'
+  if isinstance(data, list):
+    the_corpus = pd.DataFrame(data, columns=['text'])
+    col = 'text'
+  if isinstance(data, pd.DataFrame):
+    if col == "not_set_yet" : assert print("please set which column you want to check")
+    the_corpus = data
+
+  rare_words = Counter([item for row in the_corpus[col] for item in row.split(" ")])
+  rare_words_df = pd.DataFrame(rare_words.most_common())
+  rare_words_df.columns = ['word', 'count']
+  rare_words_df['word'] = rare_words_df.word.apply(lambda x : get_display(arabic_reshaper.reshape(x) ))
+  sns.set(style='dark')
+  g = sns.barplot(y="word", x="count", data=rare_words_df.sort_values(by=['count']).iloc[from_row:to_row])
+  fig = g.get_figure()
+  # bbox_inches is for showing all the plot parts.
+  if not os.path.exists('./plots_images/'):
+    os.makedirs('./plots_images/')
+  # bbox_inches is for showing all the plot parts.
+  fig.savefig(f'./plots_images/{output_name}.jpg' , bbox_inches="tight")
+  return "plot is ready"
