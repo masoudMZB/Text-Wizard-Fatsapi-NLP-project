d75cf5f94fced14c27f0074b614e21c471d24844
masoudmzb <masoudparpanchi@gmail.com>
2021-11-29 13:39:25 +0330

    doc added

--- features/data_augmentation_features_masoud.py
+++ features/data_augmentation_features_masoud.py
@@ -1,6 +1,16 @@
 from mtranslate import translate
 
 def back_translate(text ,lang_list = ["fa", "en", "fa"]):
+  """
+  this function will plot most reapeted ngrams for you
+  parameters : 
+    text : send your text in your source language. default is persian
+
+    lang_list : send a list of languages BUT BECAREFUL first index and last index should be same
+    make sure you write langs in proper format for example English is en
+
+  """
+
   augmented_text = text
   for i, lang in enumerate(lang_list):
     if i+1 < len(lang_list) :
